d01dd11a9cc98cee3586142d62769bac96021a32
burne jesica <jesicaburne@hotmail.com>
2022-05-22 18:28:10 -0300

    button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title:string = 'Mi lista';
   constructor() { }
 
   ngOnInit(): void {
